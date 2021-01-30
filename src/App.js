4763769662e31b48a278189cf2c6b7a5ef3fcac8
Ryan Skog <ryan.skog@outlook.com>
2021-01-29 23:47:49 -0800

    Initial React files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
